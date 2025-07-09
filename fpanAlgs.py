041f5d5f38597c2f6ae62baf14bd7d2a434f2859
dkhangsar <khangsardylan@gmail.com>
2025-07-09 10:26:27 -0500

    Corrected Algorithms & Testing!

---
+++ fpanAlgs.py
@@ -0,0 +1,124 @@
+import math
+
+# Testing Multiplcation
+
+class DDFloat:
+    def __init__(self, x0: float, x1: float):
+        self.x0 = x0
+        self.x1 = x1
+    
+    def __add__(self, rhs):  # representing the ddadd function
+        assert isinstance(rhs, DDFloat), f"Second variable is not a DDFloat, got {type(rhs)}"
+        z0, z1 = ddadd(self.x0, self.x1, rhs.x0, rhs.x1)
+        return DDFloat(z0, z1)
+    
+    def __mul__(self, rhs):
+        assert isinstance(rhs, DDFloat), f"Second variable is not a DDFloat, got {type(rhs)}"
+        z0, z1 = ddmul(self.x0, self.x1, rhs.x0, rhs.x1)
+        return DDFloat(z0, z1)
+
+class MFloat:
+    def __init__(self, x0: float, x1: float):
+        self.x1 = x1
+        self.x0 = x0
+
+    def __add__(self, rhs):
+        assert isinstance(rhs, MFloat), f"Second variable is not a MFloat, got {type(rhs)}"
+        z0, z1 = madd(self.x0, self.x1, rhs.x0, rhs.x1)
+        return MFloat(z0, z1)
+
+    def __mul__(self, rhs):
+        assert isinstance(rhs, MFloat), f"Second variable is not a MFloat, got {type(rhs)}"
+        z0, z1 = mmul(self.x0, self.x1, rhs.x0, rhs.x1)
+        return MFloat(z0, z1)
+
+def twoSum(a, b):
+    s = a + b
+    aEff = s - b  # amount of a in the truncated s
+    bEff = s - aEff  # ^ amount of b
+    aErr = a - aEff  # a's error
+    bErr = b - bEff
+    e = aErr + bErr  # total error
+    return (s, e)
+
+    
+
+def ddadd(x0, x1, y0, y1):
+    (a0, a1) = twoSum(x0, y0)
+    (b0, b1) = twoSum(x1, y1)
+    (c0, c1) = twoSum(a1, b0)   # c1 is discarded (infinitesimal)
+    (d0, d1) = twoSum(a0, c0)
+    (e0, e1) = twoSum(d1, b1)   # e1 discarded
+    (z0, z1) = twoSum(d0, e0) 
+    return (z0, z1)
+
+
+def madd(x0, x1, y0, y1):
+    (a0, a1) = twoSum(x0, y0)
+    (b0, b1) = twoSum(x1, y1)
+    (c0, c1) = twoSum(a0, b0)
+    (d0, d1) = twoSum(a1, b1)   # d1 discarded infinitesimal
+    (e0, e1) = twoSum(d0, c1)   # e1 discarded
+    (z0, z1) = twoSum(c0, e0)
+    return (z0, z1)
+
+
+
+
+#  Splits 53-bit IEEE double precision floating point number into a_hi, a_lo, such that a = a_hi + a_lo * a_hi
+def split(a): 
+    t =(2**27 + 1) * a
+    a_hi = t - (t - a)
+    a_lo = a - a_hi
+    return (a_hi, a_lo)
+
+
+def twoProd(a, b):
+    p = a * b
+    (ah, al) = split(a)
+    (bh, bl) = split(b)
+    e = ((ah * bh - p) + ah * bl + al * bh) + al * bl
+    return (p, e)
+
+
+
+def ddmul(x0, x1, y0, y1):
+    p00, e00 = twoProd(x0, y0)
+    p01, e01 = twoProd(x0, y1)
+    p10, e10 = twoProd(x1, y0)
+    p11, e11 = twoProd(x1, y1)
+    t1, t2 = twoSum(p00, p01 + p10)
+    return twoSum(t1, t2 + e00 + p11)
+
+
+def mmul(x0, x1, y0, y1):
+    p00, e00 = twoProd(x0, y0)
+    p01, e01 = twoProd(x0, y1)
+    p10, e10 = twoProd(x1, y0)
+    p11, e11 = twoProd(x1, y1)
+    temp = p01 + p10
+    return twoSum(p00, e00 + temp)
+
+
+
+
+
+# function & test below not actually necessary for actual twoSum computation -- but will keep just if we want to test anything!
+
+    # # Test for both numbers having same number of decimals
+    # digits1 = sum(c.isdigit() for c in str(a))
+    # digits2 = sum(c.isdigit() for c in str(b))
+    # if not digits1 == digits2 or not digits1 == numDigits:
+    #     raise ValueError("Incorrect number of digits on one of inputs")
+
+def truncateNDigits(x: float, n: int) -> float:
+    """
+    Truncate x so that its total number of digits is at most n.
+    """
+    if x == 0:
+        return 0.0
+    magnitude = math.floor(math.log10(abs(x)))  # (100->2, 9->0, etc)
+    # how much to scale so that truncating leaves n digits
+    factor = 10 ** (n - 1 - magnitude)
+    # shift, truncate toward zero, then shift back
+    return math.trunc(x * factor) / factor
---
+++ testing.py
@@ -0,0 +1,106 @@
+from fpanAlgs import twoSum, split, twoProd, ddadd, madd, ddmul, mmul, DDFloat, MFloat
+from fractions import Fraction
+from decimal import Decimal, getcontext
+
+# Test the 5 different datatypes for multiplication (evaluating on a common expression a*b + c*d*e)
+
+def evalFloat(a, b, c, d, e):
+    return a*b + c*d*e
+
+def evalFrac(a, b, c, d, e):
+    A, B, C, D, E = map(Fraction, (a, b, c, d, e))
+    return A*B + C*D*E
+
+getcontext().prec = 50
+def evalDecimal(a, b, c, d, e):
+    A, B, C, D, E = map(Decimal, (a, b, c, d, e))
+    return A*B + C*D*E
+
+def evalDD(a, b, c, d, e):
+    return DDFloat(a, 0) * DDFloat(b, 0) + DDFloat(c, 0) * DDFloat(d, 0) * DDFloat(e, 0)
+
+def evalM(a, b, c, d, e):
+    return MFloat(a, 0) * MFloat(b, 0) + MFloat(c, 0) * MFloat(d, 0) * MFloat(e, 0)
+
+
+
+
+
+
+TEST_CASES = [
+    (8.834235326183948e71,  9.578097123275567e53,
+     -4.6316785776358486e77, -2.55523157298961e61,
+     -4.6316697434005227e77, -2.037703193040189e60,
+     -4.6316697434005227e77, -2.0377031930401882e60),
+
+    (1.4919697328456323e87, 5.521397077432451e70,
+     4.973223565419703e86, -4.909094196809657e-91,
+     1.9892920893876027e87, -4.909094196809657e-91,
+     1.9892920893876027e87,  0.0),
+
+    (-8191.992462158203,     9.143899130258214e-100,
+     -9.14613152750686e-100, 2.242825269339505e-117,
+     -8191.992462158203,    -2.2323972486468995e-103,
+     -8191.992462158203,    -2.232397248646922e-103),
+
+    (-6.344854596578372e-117, 3.5220749571797036e-133,
+     4.417117661946964e71,   6.502419267294956e-117,
+     4.417117661946964e71,    1.5756467071658512e-118,
+     4.417117661946964e71,    1.5756467071658477e-118),
+
+    (-1.6110451730902522e60,  1.3071815033235768e39,
+     -2.1567956686498734e68,  1.1972619985767064e52,
+     -2.156795684760325e68,  -4.863878519471706e51,
+     -2.156795684760325e68,  -4.8638785194717065e51),
+
+    (8.84443087277937e-75,   -2.4072299075463057e-91,
+     1.7413332937543717e45, -8.843436600161344e-75,
+     1.7413332937543717e45,   9.942726180261331e-79,
+     1.7413332937543717e45,   9.942726180263738e-79),
+]
+
+if __name__ == "__main__":
+    # for i, (x0, x1, y0, y1, dd0_exp, dd1_exp, m0_exp, m1_exp) in enumerate(TEST_CASES, 1):   # syntax starts i at 1, but still is using each case and forgetting the 0th
+    #     dd0_act, dd1_act = ddadd(x0, x1, y0, y1)
+    #     m0_act,  m1_act  = madd(x0, x1, y0, y1)
+
+    #     print(f"\n=== Case #{i} ===")
+    #     print(" Inputs: x0=", x0, " x1=", x1, " y0=", y0, " y1=", y1)
+    #     print(" ddadd → actual:   ", (dd0_act, dd1_act))
+    #     print("        expected: ", (dd0_exp, dd1_exp))
+    #     print(" match? ", (dd0_act, dd1_act) == (dd0_exp, dd1_exp))
+
+    #     print(" madd  → actual:   ", (m0_act, m1_act))
+    #     print("        expected: ", (m0_exp, m1_exp))
+    #     print(" match? ", (m0_act, m1_act) == (m0_exp, m1_exp))
+
+
+    getcontext().prec = 50
+
+    a, b, c, d, e = 1.23, 4.56, 7.89, 0.12, 3.45
+
+    truth = evalDecimal(a, b, c, d, e)
+
+    for name, fn in [
+        ("Float",    evalFloat),
+        ("Fraction", evalFrac),
+        ("DDFloat",  evalDD),
+        ("MFloat",   evalM),
+    ]:
+        out = fn(a, b, c, d, e)
+
+        if hasattr(out, "x0") and hasattr(out, "x1"):
+            # not using high precision can cause the error precision to actually not get added! (i.e 1e16 + 1 goes unnoticed)
+            Dx0 = Decimal(out.x0)
+            Dx1 = Decimal(out.x1)
+            val = Dx0 + Dx1    
+        elif isinstance(out, Fraction):
+            val = Decimal(out.numerator) / Decimal(out.denominator)
+        else:
+            val = Decimal(out)
+        # Converting it into a decimal allows for highest precision
+        err = abs(val - truth)
+
+        print(f"{name:<8} -> {val:.10E}, error = {err:.2E}")
+        # : gives format specifiers (< is left alight w/ number being field width, .num is precision, E is in scientific/exponential notation)
+    
